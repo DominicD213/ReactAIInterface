663c0400dd497e237f5d30fc5cda757831d1b4c4
Domininc Digiacomo <dominicdigiacomo16@gmail.com>
2024-05-16 21:34:05 -0400

    start of project

---
+++ reactai/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-3xl text-gray-700 font-bold mb-5'>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
