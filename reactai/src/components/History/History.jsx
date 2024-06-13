a539a5070f09b2c617785c2eaf141297e9e6d31c
Domininc Digiacomo <dominicdigiacomo16@gmail.com>
2024-06-12 23:41:36 -0400

    fixing search bar

--- reactai/src/components/History/History.jsx
+++ reactai/src/components/History/History.jsx
@@ -7,12 +7,12 @@ const History = () => {
       <div className='ml-10'>
         <h1 className='font-bold' style={{ color: 'white' }}>History</h1>
         <form>
-          <div className='bg-light-grey rounded-lg mt-2 w-11/12 h-10 flex items-center'>
+          <div className='bg-light-grey rounded-lg mt-2 w-auto h-10 flex items-center mr-4'>
             <button className='h-full px-3' type='submit'>
-              <img className='h-5 w-5' src={Search} alt='search' />
+              <img className='h-5 w-5 min-w-5 min-h-5' src={Search} alt='search' />
             </button>
             <input
-              className='flex h-full bg-light-grey outline-none px-2 text-white'
+              className='flex h-full bg-light-grey outline-none mx-auto text-white w-auto'
               type='text'
               placeholder='Search history'
               style={{ color: 'white' }}
